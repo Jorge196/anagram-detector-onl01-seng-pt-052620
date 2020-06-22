a959859747d058b68c964233f7ac08833018692a
Jorge Ramirez <joramirez96@gmail.com>
2020-06-22 20:56:10 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,9 +6,9 @@ class Anagram
     @word = word
   end
 
-  def match(word_array)
-    word_array.select do |word|
-      word.split("").sort == @word.split("").sort
-    end
-  end
-end 
+  # def match(word_array)
+  #   word_array.select do |word|
+  #     word.split("").sort == @word.split("").sort
+  #   end
+  # end
+end
